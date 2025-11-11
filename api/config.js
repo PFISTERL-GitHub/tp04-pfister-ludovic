d0fb81e3db808ac347fe0eb6d31bda332ea8b876
PFISTER Ludovic <ludovicp2005@gmail.com>
2025-11-11 22:31:40 +0100

    initial commit with api

---
+++ api/config.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    ACCESS_TOKEN_SECRET : "EMMA123",
+    BDD : {
+    "host" :"dpg-d41mvgs9c44c73d005gg-a.oregon-postgres.render.com",
+    "port" : "5432",
+    "user" : "tp04_pollution_user",
+    "password" : "kfuSEkdeoLp3WiNgqQPOw3gsrKrV9frB",
+    "bdname" :"tp04_pollution" 
+    }
+}
+
